280ed79e9c3254aa6eaeddcc7f6fd75d84095260
james kiongo <jameskiongo@jamess-MacBook-Air.local>
2024-08-31 09:40:36 +0300

    started backend

---
+++ backend/api/models.py
@@ -0,0 +1,71 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class User(AbstractUser):
+    """User Model"""
+
+    email = models.EmailField(unique=True)
+    phone_no = models.CharField(max_length=10)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name", "phone_no", "email"]
+
+    def __str__(self):
+        return self.email
+
+
+class Event(models.Model):
+    """Event Model"""
+
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location_name = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class RSVP(models.Model):
+    event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE,
+        related_name="rsvps",
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="rsvps"
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=10,
+        choices=[("Going", "Going"), ("Not Going", "Not Going")],
+        default="Going",
+    )
+
+    class Meta:
+        unique_together = ("event", "user")
+
+    def __str__(self):
+        return f"{self.user} RSVP'd {self.status} for {self.event}"
+
+
+class EventImage(models.Model):
+    event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE,
+        related_name="images",
+    )
+    image = models.ImageField(upload_to="event_images/")
+
+    def __str__(self):
+        return f"Image for {self.event.title}"
