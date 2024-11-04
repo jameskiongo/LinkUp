19dac6adeac0cdb10053f45daf772e2ea8dbe939
james kiongo <jameskiongo@jamess-MacBook-Air.local>
2024-11-04 14:59:44 +0300

    added slash to user_account url registration path

--- backend/user_account/urls.py
+++ backend/user_account/urls.py
@@ -2,5 +2,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("dj_rest_auth.urls")),
-    path("registration", include("dj_rest_auth.registration.urls")),
+    path("registration/", include("dj_rest_auth.registration.urls")),
 ]
