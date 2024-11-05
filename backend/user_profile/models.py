ead88e32c48d7a5ce4d2405b336847a65a7695fe
jameskiongo <kiongojames2000@gmail.com>
2024-11-05 12:29:52 +0300

    added models to userprofile

--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+def upload_to(instance, filename):
+    return "images/{filename}".format(filename=filename)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField("auth.User", on_delete=models.CASCADE)
+    bio = models.TextField
+    profile_pic = models.ImageField(upload_to=upload_to, blank=True, null=True)
