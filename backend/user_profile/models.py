f04003db2ba2464bec56cdd9f202fef8a34d4799
jameskiongo <kiongojames2000@gmail.com>
2024-11-05 13:37:32 +0300

    added signals to create profile when user creates account

--- backend/user_profile/models.py
+++ backend/user_profile/models.py
@@ -1,3 +1,4 @@
+from allauth.socialaccount.models import SocialAccount
 from django.conf import settings
 from django.db import models
 
@@ -9,5 +10,15 @@ def upload_to(instance, filename):
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    bio = models.TextField
     profile_pic = models.ImageField(upload_to=upload_to, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.email
+
+    def save(self, *args, **kwargs):
+        if not self.profile_pic:
+            if SocialAccount.objects.filter(user=self.user).exists():
+                social_account = SocialAccount.objects.get(user=self.user)
+                self.avatar_url = social_account.extra_data.get("picture", "")
+
+        super().save(*args, **kwargs)
